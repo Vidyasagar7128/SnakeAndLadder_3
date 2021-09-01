ae1eddb633e1e0eeba28785b254916e901c31643
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-01 22:09:42 +0530

    Added Snake And Ladder

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        int user1 = 0;
+        int count = 0;
+        List<int> countStop1 = new List<int>();
+        void firstDies()
+        {
+            Console.WriteLine("Snake And Ladders...");
+        }
+        
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.firstDies();
+        }
+    }
+}
